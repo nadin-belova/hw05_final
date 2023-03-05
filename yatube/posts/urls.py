411fb4f737ed05c07e871472c28d1dc947b0bf8c
Надежда Белова <belovanadezhda1983@gmail.com>
2023-03-05 13:53:49 +0300

    Add follow/ profile/usrname/[un]follow/

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,12 +7,23 @@ app_name = "posts"
 urlpatterns = [
     path("", cache_page(20)(views.index), name="index"),
     path("group/<slug:slug>/", views.group_posts, name="group_list"),
-    # path('group_list', views.group_list, name='group_list'),
     path("profile/<str:username>/", views.profile, name="profile"),
     path("posts/<int:post_id>/", views.post_detail, name="post_detail"),
     path("create/", views.create_post, name="create_post"),
     path("posts/<int:post_id>/edit/", views.post_edit, name="post_edit"),
     path('posts/<int:post_id>/comment/',
          views.add_comment,
-         name='add_comment')
+         name='add_comment'),
+    
+    path('follow/', views.follow_index, name='follow_index'),    
+    path(
+        'profile/<str:username>/follow/',
+        views.profile_follow, 
+        name='profile_follow'
+    ),
+    path(
+        'profile/<str:username>/unfollow/',
+        views.profile_unfollow,
+        name='profile_unfollow'
+    ),
 ]
