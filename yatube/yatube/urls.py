01c19a220349431fe3048cc59cd8359faa9c74ad
Надежда Белова <belovanadezhda1983@gmail.com>
2023-03-05 20:07:07 +0300

    Add handler403

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     # path('__debug__/', include('debug_toolbar.urls')),  # delete this.RGenius
 ]
 handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.csrf_failure'
 
 
 if settings.DEBUG:
